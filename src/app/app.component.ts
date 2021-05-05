03dea37872add01e491831aa1367b692d1b3d7ac
Court Reeves <courtreeves6@gmail.com>
2021-05-05 18:13:13 -0400

    Finishing up interview questions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'interview';
+  list: Array<string> = ['test1', 'test2', 'test3', 'test4', 'test5', 'test6', 'test7', 'test8', 'test9', 'test10'];
 }
